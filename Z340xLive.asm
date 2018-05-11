608fb2e14e16a7ab4279059d0a18c94121326e24
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-11 16:45:20 -0500

    Update Z340xLive.asm

--- Z340xLive.asm
+++ Z340xLive.asm
@@ -1,9 +1,16 @@
-;;kernel.asm
-bits 32			;nasm directive - 32 bit
+;;Noot! Noot!
+
+;nasm directive - 32 bit
+bits 32
 section .text
+        ;multiboot spec
+        align 4
+        dd 0x1BADB002            ;magic
+        dd 0x00                  ;flags
+        dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
-extern kernel	        ;kmain is defined in the c file
+extern kmain	        ;kmain is defined in the c file
 
 start:
   cli 			;block interrupts
