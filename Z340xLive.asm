c72bdba6488884bc87fcb36677e6807f58e1aa22
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-11 16:36:44 -0500

    Update and rename BootLoader.asm to Z340xLive.asm

--- Z340xLive.asm
+++ Z340xLive.asm
@@ -1,6 +1,7 @@
 	BITS 16
 
 start:
+	text_string db '[nootloader@core] Staring down Z340x...', 0
 	mov ax, 07C0h		; Set up 4K stack space after this bootloader
 	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
 	mov ss, ax
@@ -16,7 +17,9 @@ start:
 	jmp $			; Jump here - infinite loop!
 
 
-	text_string db 'NootLoader (Z340x, enter boot menu to change OS)', 0
+	text_string db '[nootloader@core] Ready', 0
+	text_string db '[Z340x@Z340x] Welcome to Z340x - Boot Conditions: live, single-user...', 0 		; Welcome
+	text_string db '[Z340x@Z340x] Distrobution: Z340x', 0 		; Say Distrobution
 
 
 print_string:			; Routine: output string in SI to screen
