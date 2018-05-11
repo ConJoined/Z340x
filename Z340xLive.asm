3361a5e12afeb8d530191db641157f16cfef6e36
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-11 16:42:41 -0500

    Update Z340xLive.asm

--- Z340xLive.asm
+++ Z340xLive.asm
@@ -3,7 +3,7 @@ bits 32			;nasm directive - 32 bit
 section .text
 
 global start
-extern kmain	        ;kmain is defined in the c file
+extern kernel	        ;kmain is defined in the c file
 
 start:
   cli 			;block interrupts
